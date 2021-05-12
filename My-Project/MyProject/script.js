307500e8245a18dd463655a5b8aef39b634671ce
qurbaniemrah <qurbaniemrah@outlook.com>
2021-05-12 15:35:29 +0400

    section top nav scroll isleri goruldu

--- My-Project/MyProject/script.js
+++ My-Project/MyProject/script.js
@@ -1,15 +1,33 @@
+// top menu section start
 let containerHeader = document.getElementById('container-header');
-let miniContainerHeader = document.getElementById('mini-container-header');
-let scrollVerticall = window.scrollY;
+let miniNavBar = document.getElementById('mini-nav-bar');
+let logo = document.getElementsByClassName('logo');
+let socialConnect = document.getElementsByClassName('social-connect');
+let navBar = document.getElementsByClassName('nav-bar');
 
-if (scrollVerticall = containerHeader.style.height =  '200px') {
-    containerHeader.style.display = 'none'
-}else {
-    containerHeader.style.display = 'block'
+
+window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+        containerHeader.style.display = 'none';
+        miniNavBar.style.display = 'block';
+        miniNavBar.style.position = 'fixed';
+        miniNavBar.style.top = 0;
+        miniNavBar.style.left = 0;
+    } else if (window.pageYOffset<200) {
+        containerHeader.style.display = 'block';
+        miniNavBar.style.display = 'none';
+        containerHeader.style.position = 'fixed';
+        containerHeader.style.top = 0;
+        containerHeader.style.backgroundColor = '#FFFFFF';
+        containerHeader.style.display = 'flex';
+        containerHeader.style.width = '100%';
+        containerHeader.style.margin = '0px';
+    } else {
+        
+    } 
+        
+        
+    
 }
 
-
-
-
-
-
+// top menu section end
