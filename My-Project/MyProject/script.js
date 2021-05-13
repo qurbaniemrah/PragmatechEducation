5a130e761a7583587a57818e4eb94aac152e7d76
qurbaniemrah <qurbaniemrah@outlook.com>
2021-05-13 06:00:03 +0400

    kodlari seliqeye salma isleri goruldu

--- My-Project/MyProject/script.js
+++ My-Project/MyProject/script.js
@@ -4,7 +4,7 @@ let miniNavBar = document.getElementById('mini-nav-bar');
 let logo = document.getElementsByClassName('logo');
 let socialConnect = document.getElementsByClassName('social-connect');
 let navBar = document.getElementsByClassName('nav-bar');
-
+let slideDirection = document.getElementsByClassName('slide-direction');
 
 window.onscroll = function () {
     if (window.pageYOffset > 200) {
@@ -13,6 +13,7 @@ window.onscroll = function () {
         miniNavBar.style.position = 'fixed';
         miniNavBar.style.top = 0;
         miniNavBar.style.left = 0;
+        slideDirection.style.opacity = 0;
     } else if (window.pageYOffset<200) {
         containerHeader.style.display = 'block';
         miniNavBar.style.display = 'none';
@@ -22,12 +23,11 @@ window.onscroll = function () {
         containerHeader.style.display = 'flex';
         containerHeader.style.width = '100%';
         containerHeader.style.margin = '0px';
-    } else {
-        
-    } 
-        
-        
-    
-}
+    }
+      }
 
 // top menu section end
+
+// slider section start
+
+// slider section end
