6ef053b156aac16843bd080c05e01a9ce9bb5c98
qurbaniemrah <qurbaniemrah@outlook.com>
2021-05-16 05:14:20 +0400

    container header section uzerinde usler gorulfu

--- My-Project/MyProject/script.js
+++ My-Project/MyProject/script.js
@@ -4,7 +4,7 @@ let miniNavBar = document.getElementById('mini-nav-bar');
 let logo = document.getElementsByClassName('logo');
 let socialConnect = document.getElementsByClassName('social-connect');
 let navBar = document.getElementsByClassName('nav-bar');
-let slideDirection = document.getElementsByClassName('slide-direction');
+
 
 window.onscroll = function () {
     if (window.pageYOffset > 200) {
@@ -14,7 +14,7 @@ window.onscroll = function () {
         miniNavBar.style.top = 0;
         miniNavBar.style.left = 0;
         slideDirection.style.opacity = 0;
-    } else if (window.pageYOffset<200) {
+    } else if (window.pageYOffset < 200) {
         containerHeader.style.display = 'block';
         miniNavBar.style.display = 'none';
         containerHeader.style.position = 'fixed';
@@ -24,10 +24,54 @@ window.onscroll = function () {
         containerHeader.style.width = '100%';
         containerHeader.style.margin = '0px';
     }
-      }
+}
 
 // top menu section end
 
 // slider section start
+
+
+// let slide = document.getElementsByClassName('slide');
+// let slideDirection = document.getElementsByClassName('slide-direction');
+// let slideLeft = document.getElementsByClassName('slide-left');
+// let slideRight = document.getElementsByClassName('slide-right');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // slider section end
