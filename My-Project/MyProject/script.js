a2d6166243856f5ca6fb52a218bd1aa65c6a8031
qurbaniemrah <qurbaniemrah@outlook.com>
2021-05-29 10:52:01 +0400

    container header section ve javascriptdeki problem hell oldu

--- My-Project/MyProject/script.js
+++ My-Project/MyProject/script.js
@@ -4,26 +4,29 @@ let miniNavBar = document.getElementById('mini-nav-bar');
 let logo = document.getElementsByClassName('logo');
 let socialConnect = document.getElementsByClassName('social-connect');
 let navBar = document.getElementsByClassName('nav-bar');
+let containerHeaderImages = document.querySelectorAll('#container-header img');
 
 
 window.onscroll = function () {
-    if (window.pageYOffset > 200) {
+    if (window.pageYOffset > 190) {
         containerHeader.style.display = 'none';
         miniNavBar.style.display = 'block';
         miniNavBar.style.position = 'fixed';
         miniNavBar.style.top = 0;
         miniNavBar.style.left = 0;
+       
 
-    } else if (window.pageYOffset < 200) {
+    } else if (window.pageYOffset < 190) {
         containerHeader.style.display = 'block';
         miniNavBar.style.display = 'none';
-        containerHeader.style.position = 'fixed';
+        containerHeader.style.display = 'relative';
         containerHeader.style.top = 0;
         containerHeader.style.backgroundColor = '#FFFFFF';
         containerHeader.style.display = 'flex';
         containerHeader.style.width = '100%';
         containerHeader.style.margin = '0px';
-    }
+       
+    } 
 }
 
 // top menu section end
@@ -56,7 +59,7 @@ rightButton.onclick = () => {
 
 leftButton.onclick = () => {
     leftValue -= imagesWidth
-    if (leftValue < (images.length - 1) * imagesWidth) {
+    if (leftValue < 0) {
         leftValue = 0;
         leftValue = (images.length-1)*imagesWidth
     }
