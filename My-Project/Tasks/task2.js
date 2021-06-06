7dc05b2dc170c122659852b6d740e14d9db5376e
qurbaniemrah <qurbaniemrah@outlook.com>
2021-06-06 14:58:08 +0400

    task2 tamamlandi

---
+++ My-Project/Tasks/task2.js
@@ -0,0 +1,43 @@
+let firstValue = prompt('Enter your first value :');
+let secondValue = prompt('Enter your last value');
+
+let a = firstValue;
+let b = secondValue;
+
+
+if(a>b) {
+    alert('dogru ededi daxol edin');
+}else {
+    alert('daxil edilen deyer sefdir');
+}
+
+
+
+function topla(a,b) {
+    return 7 + 10;
+    
+}
+
+alert(topla(a,b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
