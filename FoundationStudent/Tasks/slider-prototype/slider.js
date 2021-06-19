b1e197eb44b89e71eae1b6a554e77470cfecc4b1
qurbaniemrah <qurbaniemrah@outlook.com>
2021-06-19 17:29:36 +0400

    javascript hissede isler qismen goruldu

---
+++ FoundationStudent/Tasks/slider-prototype/slider.js
@@ -0,0 +1,34 @@
+let img = document.querySelector('.slider-frame img');
+let leftBtn = 
+img.setAttribute('src','./img/slider1.jpg','./img/slider2.jpg','./img/slider3.jpg');
+
+
+
+
+
+let imgSources = [
+    '.img/slider1.jpg',
+    '.img/slider2.jpg',
+    '.img/slider2.jpg'
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
