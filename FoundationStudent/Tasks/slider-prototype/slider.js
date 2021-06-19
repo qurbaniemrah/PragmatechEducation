fb5100bba95139d8589e30d485331380acf74562
qurbaniemrah <qurbaniemrah@outlook.com>
2021-06-19 22:57:16 +0400

    slider sekil hisse tamamlandi

--- FoundationStudent/Tasks/slider-prototype/slider.js
+++ FoundationStudent/Tasks/slider-prototype/slider.js
@@ -1,16 +1,46 @@
 let img = document.querySelector('.slider-frame img');
-let leftBtn = 
-img.setAttribute('src','./img/slider1.jpg','./img/slider2.jpg','./img/slider3.jpg');
-
-
-
-
-
-let imgSources = [
-    '.img/slider1.jpg',
-    '.img/slider2.jpg',
-    '.img/slider2.jpg'
+let imgSrc = [
+    'img/slider1.jpg',
+    'img/slider2.jpg',
+    'img/slider3.jpg'
 ]
+let leftBtn = document.getElementsByClassName('left');
+let rightBtn = document.getElementsByClassName('right');
+let count =0;
+
+leftBtn[0].addEventListener('click', function () {
+  if (count <1) {
+    img.setAttribute('src', imgSrc[2]);
+    count =2;
+  }else {
+    img.setAttribute('src', imgSrc[count]);
+    count --
+  }
+})
+
+rightBtn[0].addEventListener('click', function () {
+   if (count>2) {
+    img.setAttribute('src', imgSrc[0]);
+    count = 0;
+   }else {
+    img.setAttribute('src', imgSrc[count]);
+    count ++
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
