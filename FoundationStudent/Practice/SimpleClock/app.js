e55763de9c2ce7b8ce3db47e71f4c179fbea718b
qurbaniemrah <qurbaniemrah@outlook.com>
2021-06-27 13:16:53 +0400

    js clock javascript hisse qismen edildi

---
+++ FoundationStudent/Practice/SimpleClock/app.js
@@ -0,0 +1,21 @@
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+
+function increaseHours () {
+
+}
+
+function increaseMinutes () {
+    
+}
+
+function increaseSeconds () {
+    
+}
+
+
+
+
+
+
