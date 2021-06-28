d6db3df6e621e2306b39851f0a67740c9a12ce48
qurbaniemrah <qurbaniemrah@outlook.com>
2021-06-28 08:23:09 +0400

    color flipper qismen tamamlandi

--- FoundationStudent/Practice/ColorFlipper/app.js
+++ FoundationStudent/Practice/ColorFlipper/app.js
@@ -1,9 +1,19 @@
 let colorContainer = document.querySelector('.color-container');
 let colorContent = document.querySelector('.color-content');
 let btn = document.querySelector('.btn');
+let red = Math.floor(Math.random() * 256);
+let green = Math.floor(Math.random() * 256);
+let blue = Math.floor(Math.random() * 256);
+let startValue = 0;
 
-console.log(colorContainer);
 
-btn.addEventListener('click', function () {
-  colorContainer.style.backgroundColor = 'red';
+
+function changeColor() {
+  colorContainer.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+changeColor();
+
+btn.addEventListener('click', () => {
+  changeColor();
 })
