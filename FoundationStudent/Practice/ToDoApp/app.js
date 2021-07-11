8958a95368c9d67f124408ec771051474ec19702
qurbaniemrah <qurbaniemrah@outlook.com>
2021-07-11 17:39:27 +0400

    todo list qismen tamamlandi

---
+++ FoundationStudent/Practice/ToDoApp/app.js
@@ -0,0 +1,14 @@
+let todoContent = document.querySelector('.todo-content');
+let button = document.querySelector('.submit');
+let value = todoContent.value;
+let zero  = 0;
+
+
+
+button.addEventListener('click',()=>{
+  
+    console.log(todoContent.value);
+})
+
+
+
