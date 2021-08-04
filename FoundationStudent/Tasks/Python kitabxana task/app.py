ce6451f2facc2f43f4876e4e49edb44571dc92af
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-04 05:08:55 +0400

    books class yaradildi

--- FoundationStudent/Tasks/Python kitabxana task/app.py
+++ FoundationStudent/Tasks/Python kitabxana task/app.py
@@ -10,6 +10,21 @@ class Book:
           self.Pages = _pages
           self.Title = _title
           self.Year = _year
-          
+
+def findBookByName(_bookname):
+    # kitab adı daxil edildiyi zaman kitabın digər detallarını ekrana çap edin
+       pass
+
+def totalPaper():
+    # bütün siyahıda var olan kitabların səhifə saylarının cəmini ekrana çap edin
+       pass
+
+def deleteByCountry(_countryName):
+    # ölkə adı daxil edildiyi zaman o ölkəyə aid olan kitabların json faylından silinməsini təmin edin
+       pass
+
+def convertToList():
+    # bütün kitabların list>object məntiqinə əsasən çevirin
+       pass          
  
     
