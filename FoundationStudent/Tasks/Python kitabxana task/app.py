b3349f0121835781a96727d965d92b33e53e16d4
qurbaniemrah <qurbaniemrah@outlook.com>
2021-08-04 05:08:15 +0400

    books class yaradildi

---
+++ FoundationStudent/Tasks/Python kitabxana task/app.py
@@ -0,0 +1,15 @@
+books = []
+
+class Book:
+     def __init__(self,_author,_country,_imagelink,_language,_link,_pages,_title,_year):
+          self.Author = _author
+          self.Country = _country
+          self.ImageLink = _imagelink
+          self.Language = _language
+          self.Link = _link
+          self.Pages = _pages
+          self.Title = _title
+          self.Year = _year
+          
+ 
+    
